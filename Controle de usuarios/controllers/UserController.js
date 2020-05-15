2c952fb98e3a7ca52cbe3692939d948f6a8f7ffb
Cauê Santos <caah.sfc@live.com>
2020-05-14 23:07:40 -0300

    Validando formulários

--- Controle de usuarios/controllers/UserController.js
+++ Controle de usuarios/controllers/UserController.js
@@ -53,7 +53,12 @@ class UserController{
 
     getValues(){
         let user = {};
+        let isValid = true;
         [... this.formEl.elements].forEach(function(field){
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
             if(field.name == "gender" && field.checked){
                 user[field.name] = field.value;
             }else if(field.name == "type"){
@@ -62,7 +67,10 @@ class UserController{
                 user[field.name] = field.value;
             }
         });
-    
+        
+        if(!isValid){
+            return false;
+        }
         return new User(
             user.name, user.gender, user.birth, 
             user.country, user.email, user.password, 
